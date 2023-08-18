28df93df7f2582aa58d69c25ab224f28d3cd8ffb
Ryan <ryannhu3@gmail.com>
2023-08-17 21:24:54 -0400

    updated missing items

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,48 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Ryan Hu on 2023-08-11.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
+            }
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (3rd generation)", "iPhone 14 Pro", "iPad Air (5th generation)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .environmentObject(ModelData())
+            
+        }
+    }
+}
